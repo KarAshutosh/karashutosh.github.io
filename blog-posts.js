d9009a8b33afe755eaa97837092ecdf37ee28ce7
KarAshutosh <karanamashutosh@gmail.com>
2025-08-15 23:04:03 +0530

    Added informational tag

--- blog-posts.js
+++ blog-posts.js
@@ -4,12 +4,38 @@ window.blogPosts = [
         title: "Reflected XSS Filter Bypass in Search Functionality",
         excerpt: "Two years ago, I bypassed a site's XSS filter with a simple HTML trick, executing JavaScript in the browser. Now fixed, it's a reminder that proper encoding is key.",
         date: "2025-08-15",
-        readTime: "15 min read",
+        readTime: "3 min read",
         severity: "Medium",
         topics: ["XSS", "Security Testing", "Bug Hunting"],
         githubUrl: "blog-post.html?id=1",
         mediumUrl: "https://kashsecurity.medium.com/reflected-xss-filter-bypass-in-search-functionality-be2d511e7621",
         markdownUrl: "blogs/blog1.md",
         blogTitle: "Reflected XSS Filter Bypass in Search Functionality"
+    },
+    {
+        id: 2,
+        title: "Linux hardening for web servers",
+        excerpt: "Two years ago, I bypassed a site's XSS filter with a simple HTML trick, executing JavaScript in the browser. Now fixed, it's a reminder that proper encoding is key.",
+        date: "coming soon",
+        readTime: "3 min read",
+        severity: "Informational",
+        topics: ["XSS", "Security Testing", "Bug Hunting"],
+        githubUrl: "blog-post.html?id=1",
+        mediumUrl: "https://kashsecurity.medium.com/reflected-xss-filter-bypass-in-search-functionality-be2d511e7621",
+        markdownUrl: "blogs/blog1.md",
+        blogTitle: "Linux hardening for web servers"
+    },
+    {
+        id: 3,
+        title: "Every OWASP Juice Shop Vulnerability",
+        excerpt: "Two years ago, I bypassed a site's XSS filter with a simple HTML trick, executing JavaScript in the browser. Now fixed, it's a reminder that proper encoding is key.",
+        date: "coming soon",
+        readTime: "3 min read",
+        severity: "Informational",
+        topics: ["XSS", "Security Testing", "Bug Hunting"],
+        githubUrl: "blog-post.html?id=1",
+        mediumUrl: "https://kashsecurity.medium.com/reflected-xss-filter-bypass-in-search-functionality-be2d511e7621",
+        markdownUrl: "blogs/blog1.md",
+        blogTitle: "Every OWASP Juice Shop Vulnerability"
     }
 ];
